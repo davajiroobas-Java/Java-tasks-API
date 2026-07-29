248c635663b4994af0479538b14d93fa6571e459
Dawan <davaj.iroobas@gamil.com>
2026-07-30 01:50:29 +0200

    Create  CRUD

--- IdeaProjects/Training/src/main/java/com/example/training/Student.java
+++ IdeaProjects/Training/src/main/java/com/example/training/Student.java
@@ -1,17 +1,25 @@
 package com.example.training;
-
-
 public class Student {
 
+    private Integer id;
     private String name;
     private int age;
 
     public Student() {
     }
 
-    public Student(String name, int age) {
+    public Student(Integer id, String name, int age) {
+        this.id = id;
         this.name = name;
         this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
--- IdeaProjects/Training/src/main/java/com/example/training/controller/StudentController.java
+++ IdeaProjects/Training/src/main/java/com/example/training/controller/StudentController.java
@@ -12,25 +12,92 @@ import java.util.List;
 public class StudentController {
 
 
-@GetMapping("/hello")
-    public String hello(){
-    return  "Hello from java!";
-}
-
-@GetMapping("/hello/{name}")
-public String helloName(@PathVariable String name){
-    return "Hello " + name;
-}
 
 
-@GetMapping("/name")
-public String hello(@RequestParam String name){
-    return "Hello "+ name;
-}
+    private List<Student> students=new ArrayList<>();
+
+    private Integer nextId=1;
 
 
-@PostMapping("/students")
+    //Create student  with ID
+
+    @PostMapping("/students")
     public Student createStudent(@RequestBody Student student){
-    return student;
-}
+
+        student.setId(nextId++);
+        students.add(student);
+
+        return student;
+    }
+
+    // Fetch All Students
+
+
+
+    // Fetch Students by id and use @PathVariable(req.params)
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable Integer id){
+
+        for(Student student: students){
+            if(student.getId().equals(id)){
+                return student;
+            }
+        }
+        return null;
+    }
+
+
+    // Fetch Students by searching name and use @RequestParam(query-parameter)
+    @GetMapping("/students/search")
+    public List<Student> searchStudents(@RequestParam String name) {
+
+        List<Student> result = new ArrayList<>();
+
+        for (Student student : students) {
+            if (student.getName().equalsIgnoreCase(name)) {
+                result.add(student);
+            }
+        }
+
+        return result;
+    }
+
+
+  // To update user/student  by id use both @PathVariable and @RequestBody
+    @PutMapping("/students/{id}")
+    public Student updateStudent(@PathVariable Integer id,
+                                 @RequestBody Student updatedStudent) {
+
+        for (Student student : students) {
+
+            if (student.getId().equals(id)) {
+
+                student.setName(updatedStudent.getName());
+                student.setAge(updatedStudent.getAge());
+
+                return student;
+            }
+        }
+
+        return null;
+    }
+
+
+    //To remove a student by id, just use @PathVariable
+    @DeleteMapping("/students/{id}")
+    public String deleteStudent(@PathVariable Integer id) {
+
+        for (Student student : students) {
+
+            if (student.getId().equals(id)) {
+
+                students.remove(student);
+
+                return "Student borttagen";
+            }
+        }
+
+        return "Student hittades inte";
+    }
+
 }
